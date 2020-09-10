50af493a0fb2058edd2b9147a3e5a3ccebfee341
IPutuTiksnaWiratama <putuwiratama111@gmail.com>
2020-09-10 13:43:57 +0800

    5rd commit

---
+++ pertmuan_7/index.js
@@ -0,0 +1,15 @@
+//Java script conditional
+
+/* Menggunakan if, else if dan else
+let hargaBuah = 10000;
+if (hargaBuah > 8000){
+    console.log("Mahal");
+} else if (hargaBuah >= 5000 %% hargaBuah <= 8000) {
+    console.log("Normal");
+} else {
+    console.log("Murah");
+}
+*/
+
+//Menggunakan Swtich dan Case
+let 
