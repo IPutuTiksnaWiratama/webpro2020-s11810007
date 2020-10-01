50396e6c8cc10012d8ad1aa891786b8a49eefb04
IPutuTiksnaWiratama <putuwiratama111@gmail.com>
2020-10-01 13:57:20 +0800

    13th commit

--- pertemuan_11/index.js
+++ pertemuan_11/index.js
@@ -68,7 +68,7 @@ const yearUntilRetirement = (year = 2020, firstName = "Putu") => {
     let age = 28;
     let retirement = 65 - age;
     if (retirement > 0) {
-        console.log(firstName + " retired in " + retirement + " years ");
+        console.log(firstName + " retired in " + retirement + " years   ");
     } else {
         console.log(firstName + " is already retired.");
     }
