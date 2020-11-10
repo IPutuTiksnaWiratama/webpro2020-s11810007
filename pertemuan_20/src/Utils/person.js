060d6a61302bcf40aed92218b7f013225a2840fb
IPutuTiksnaWiratama <putuwiratama111@gmail.com>
2020-11-10 23:02:35 +0800

    pertemuan_21

---
+++ pertemuan_20/src/Utils/person.js
@@ -0,0 +1,28 @@
+  
+export const person = [
+    {
+        name: "Putu Wiratama",
+        institution: "Unklab",
+        address: "Bitung",
+        phone: "081220412615",
+    },
+    {
+        name: "Liu Bei",
+        institution: "ITB",
+        address: "China",
+        phone: "023xxxxxxx",
+    },
+    {
+        name: "Cao Cao",
+        institution: "Unsrat",
+        address: "Manado",
+        phone: "083xxxxxxx",
+    },
+    {
+        name: "Trump",
+        institution: "Loser",
+        address: "Manado",
+        phone: "082xxxxxxx",
+    },
+    ];
+    
